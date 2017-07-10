af343ab9bbcb00367ca10daa465bd37ada3996b3
Nelson Uhan <nelson@uhan.me>
2017-07-10 16:08:12 -0400

    Add PrusaControl 0.9.2.297_beta

---
+++ Casks/prusacontrol.rb
@@ -0,0 +1,9 @@
+cask 'prusacontrol' do
+  version '0.9.2.297_beta'
+
+  url "http://prusacontrol.org/update/mac"
+  name 'PrusaControl'
+  homepage 'http://prusacontrol.org'
+
+  app 'PrusaControl.app'
+end
