828721a35f47979fb0bc29368749f5eee9bb92b7
Nelson Uhan <nelson@uhan.me>
2017-07-10 16:11:29 -0400

    Update prusacontrol to include sha256 for 0.9.2.297_beta

--- Casks/prusacontrol.rb
+++ Casks/prusacontrol.rb
@@ -1,5 +1,6 @@
 cask 'prusacontrol' do
   version '0.9.2.297_beta'
+  sha256 'fe7c165df6c38b3c75352142373bd7c5637aa71dd3d7d41f9daf294219bfb4c4'
 
   url "http://prusacontrol.org/update/mac"
   name 'PrusaControl'
