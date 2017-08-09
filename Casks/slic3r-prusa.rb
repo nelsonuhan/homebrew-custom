6728e2dddcbac12892dbb3f42a2c36c2346f5599
Nelson Uhan <nelson@uhan.me>
2017-08-09 14:31:51 -0400

    Update slic3r-prusa

--- Casks/slic3r-prusa.rb
+++ Casks/slic3r-prusa.rb
@@ -1,8 +1,8 @@
 cask 'slic3r-prusa' do
-  version '1.34.1'
-  sha256 'bd85fcafe6ef23ee39330baaa9967a81f89d91fde007549622f34c3d1213fd59'
+  version '1.36.2'
+  sha256 '12d874aa6baf01062e4cd3ba3cffb1dc42a664562716c185d046215a05ecfaf0'
 
-  url "https://github.com/prusa3d/Slic3r/releases/download/version_1.34.1/Slic3r-1.34.1-prusa3d-full-201704060802.dmg"
+  url "https://github.com/prusa3d/Slic3r/releases/download/version_1.36.2/Slic3r-1.36.2-prusa3d-full-201707281835.dmg"
   appcast 'https://github.com/prusa3d/Slic3r/releases.atom',
           checkpoint: 'fa8116c71b69e948e0eec95cecf4c7837a76af9e418c483550de9659aa52a4b6'
   name 'Slic3r Prusa Edition'
