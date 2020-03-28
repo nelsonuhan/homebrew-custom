5e744e8ab2c44ca6086ac1f3b014eaeea51608e4
Nelson Uhan <nelson@uhan.me>
2020-03-28 14:32:30 -0400

    Add taccy

---
+++ Casks/taccy.rb
@@ -0,0 +1,10 @@
+cask 'taccy' do
+  version '1.8'
+  sha256 '10275f22033038bd61758d3768b3760e0f0d8ddb6940c97f89622d961c763fab'
+
+  url "https://eclecticlightdotcom.files.wordpress.com/2020/01/taccy18.zip"
+  name 'Taccy'
+  homepage 'https://eclecticlight.co/taccy-signet-precize-alifix-utiutility-alisma/'
+
+  app 'Taccy.app'
+end
